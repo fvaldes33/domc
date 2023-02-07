42321cdda018e9d67ed0e9f324f71f5440172a4d
Franco Valdes <franco.valdes89@gmail.com>
2023-02-07 00:40:16 -0500

    cors

--- src/pages/api/download-logs.ts
+++ src/pages/api/download-logs.ts
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import Cors from "cors";
 import { z } from "zod";
 
 const DownloadLogsSchema = z.object({
@@ -11,10 +12,32 @@ type Data = {
   error?: any;
 };
 
+const cors = Cors({
+  methods: ["POST", "GET", "HEAD"],
+});
+
+function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  fn: Function
+) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  await runMiddleware(req, res, cors);
+
   const validation = DownloadLogsSchema.safeParse(req.body);
   if (!validation.success) {
     return res.status(500).json({
